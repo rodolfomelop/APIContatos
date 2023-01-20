06760cf65ec05d7e3e9c3d9e0274e5825d02c5f7
Rodolfo Pires <rodolfo.melop@hotmail.com>
2023-01-20 08:04:23 -0300

    APIWEB

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MODULOAPI.Entities;
+
+namespace MODULOAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos{ get; set; }
+    }
+}
